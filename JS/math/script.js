ebc17cba815e429da8379036226020ff64c375d2
Pratham Raghuvanshi <141766108+Pratham22R@users.noreply.github.com>
2024-07-21 21:03:30 +0530

    Add files via upload

---
+++ JS/math/script.js
@@ -0,0 +1,53 @@
+const a = parseInt(4)
+const b = parseInt(6)
+
+//OPERATORS
+//Addition
+const c = a+b
+
+//Subtraction
+const d = a-b
+
+//Multiplication
+const e = a*b
+
+//Division
+const f = a/b
+
+//Remainder
+const g = a%b
+
+//Exponentiation
+const h = a**b
+
+//Math Object
+//Properties
+//PI
+const i = Math.PI
+
+//Square Root
+const j = Math.SQRT2
+
+//E
+const k = Math.E
+
+//Methods
+//Square Root
+const l = Math.sqrt(a)
+
+//Power
+const m = Math.pow(a, b)
+
+//Floor
+const a1 = 4.6
+const n = Math.floor(a1)
+
+//Ceil
+const o = Math.ceil(a1)
+
+//Round
+const p = Math.round(a1)
+
+//Random
+const q = Math.random()//To get the random number u need to reload the page again n again 
+
